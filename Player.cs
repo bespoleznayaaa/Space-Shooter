f404ee9e89443f4daf8ddbf0d0b51108d6ba3f6d
Saltanov <150438059+bespoleznayaaa@users.noreply.github.com>
2024-02-22 19:17:11 +0400

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int points;
+    public Projectile projectilePrefab;
+    public float shootInterval;
+    public float shootTimer;
+    public Transform shootPoint;
+
+    void Shoot()
+    {
+        if(shootTimer <= 0)
+        {
+             Instantiate(projectilePrefab, shootPoint.position, Quaternion.identity);
+             shootTimer = shootInterval;
+        }
+    }
+
+    void Move()
+    {
+        if(Input.GetMouseButton(0))
+        {
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 realPos = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = realPos;
+        }
+        Shoot();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        shootTimer -= Time.deltaTime;
+    }
+}
